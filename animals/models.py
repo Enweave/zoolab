100bd03985474e0b2e2ed246709adde2fe1f0512
Enweave <enweave@gmail.com>
2016-04-10 01:08:18 +0500

    update verbose names

--- animals/models.py
+++ animals/models.py
@@ -99,9 +99,9 @@ class SuppliedAnimal(models.Model):
         (u"female", u"Ж"),
     )
 
-    animal_type = models.ForeignKey(AnimalType)
-    animal_count = models.IntegerField()
-    animal_gender = models.CharField(choices=GENDERS, max_length=255)
+    animal_type = models.ForeignKey(AnimalType, verbose_name=u"Тип животного")
+    animal_count = models.IntegerField(verbose_name=u"Количество")
+    animal_gender = models.CharField(choices=GENDERS, max_length=255, verbose_name=u"Пол животного")
     supply = models.ForeignKey(Supply)
 
     def get_gender_display(self):
@@ -116,8 +116,8 @@ class SuppliedConsumable(models.Model):
     Служебный класс,
     показывает, сколько животных конкретного типа и какого пола было добавлено с указанной поставкой
     """
-    consumable_type = models.ForeignKey(ConsumableType)
-    consumable_count = models.IntegerField()
+    consumable_type = models.ForeignKey(ConsumableType, verbose_name=u"Тип потребности")
+    consumable_count = models.IntegerField(verbose_name=u"Количество")
     supply = models.ForeignKey(Supply)
 
     def __unicode__(self):
